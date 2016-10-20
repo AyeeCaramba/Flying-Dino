4f245ad7464e2aad3969bc6a1acdd2ae8e9eb273
Riley Smith <riley.smith@sea.aie.edu>
2016-10-19 17:56:26 -0700

    Started implementing planet gravity Few bugs with the PlanetManager accessing

---
+++ FlyingDino/Assets/Scripts/GravityController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GravityController : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+
+    private Transform closestPlanet
+    {
+        get
+        {
+            if (PlanetManager.instance.planetTransforms != null &&
+                PlanetManager.instance.planetTransforms.Count > 0)
+            {
+
+                float result = float.MaxValue;
+                Transform target = null;
+
+                foreach (Transform tForm in PlanetManager.instance.planetTransforms)
+                {
+                    float dist = Vector3.Distance(tForm.position, transform.position);
+                    if (dist < result)
+                    {
+                        result = dist;
+                        target = tForm;
+                    }
+                }
+
+                if (target != null) return target;
+                else return null;
+            }
+            else return null;
+        }
+    }
+
+    private Vector3 gravityDirection
+    {
+        get
+        {
+            Transform target = closestPlanet;
+
+            if (target != null)
+                return (transform.position - target.position).normalized;
+
+            else return Vector3.zero;
+        }
+    }
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ FlyingDino/Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class PlanetManager : MonoBehaviour
+{
+
+    public static PlanetManager instance { get; set; }
+
+    public static List<Transform> planetTransforms;
+
+    // Use this for initialization
+    void Start()
+    {
+        planetTransforms = new List<Transform>();
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Planet")) {
+            planetTransforms.Add(obj.transform);
+        }
+    }
+}
